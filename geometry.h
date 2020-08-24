497f7fe7b0a0058b3c1a2e7b9fdef0c84d9a42cb
mumiron <mumironk@gmail.com>
2020-08-24 19:52:33 +0700

    Add header

---
+++ geometry.h
@@ -0,0 +1,9 @@
+#ifndef GEOMETRY
+#define GEOMETRY
+
+int input();
+int calculator (int n, float radius, float all);
+int intersection (int n, float radius, float all);
+void free (float all, float radius);
+
+#endif
